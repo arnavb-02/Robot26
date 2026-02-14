56127fee487c774d2d661184d9e95c5ee51f79c1
Arnav Bhattacharya <bhattacharyaa@students.osd.wednet.edu>
2026-02-14 13:13:55 -0800

    Hopper Changes

--- src/main/java/Team4450/Robot26/subsystems/Hopper.java
+++ src/main/java/Team4450/Robot26/subsystems/Hopper.java
@@ -5,7 +5,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import java.util.ResourceBundle.Control;
+
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 
 public class Hopper extends SubsystemBase {
     private final TalonFX hopperMotor = new TalonFX(Constants.HOPPER_MOTOR_CAN_ID, new CANBus(Constants.CANIVORE_NAME));
@@ -15,12 +19,15 @@ public class Hopper extends SubsystemBase {
         hopperMotor.setNeutralMode(NeutralModeValue.Coast);
 
         SmartDashboard.putNumber("Hopper Power", Constants.HOPPER_MOTOR_POWER);
+        SmartDashboard.putNumber("Hopper Target RPS", 0);
+
         hopperMotor.set(0);
     }
 
     public void start() {
-        double power = SmartDashboard.getNumber("Hopper Power", Constants.HOPPER_MOTOR_POWER);
-        hopperMotor.set(power);
+        // double power = SmartDashboard.getNumber("Hopper Power", Constants.HOPPER_MOTOR_POWER);
+        double rps = SmartDashboard.getNumber("Hopper Target RPS", 0);
+        hopperMotor.setControl(new VelocityVoltage(rps));
     }
 
     public void stop() {
