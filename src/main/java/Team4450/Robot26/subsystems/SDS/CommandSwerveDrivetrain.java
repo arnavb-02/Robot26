c50cad564e5e54032e818f958205587b73c4ff1a
anon <anon@email.com>
2026-03-01 15:08:12 -0800

    Revert auto builder changes

--- src/main/java/Team4450/Robot26/subsystems/SDS/CommandSwerveDrivetrain.java
+++ src/main/java/Team4450/Robot26/subsystems/SDS/CommandSwerveDrivetrain.java
@@ -201,29 +201,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private void configureAutoBuilder() {
         try {
             var config = RobotConfig.fromGUISettings();
-            // AutoBuilder.configure(
-            //     () -> getState().Pose,   // Supplier of current robot pose
-            //     this::resetPose,         // Consumer for seeding pose against auto
-            //     () -> getState().Speeds, // Supplier of current robot speeds
-            //     // Consumer of ChassisSpeeds and feedforwards to drive the robot
-            //     (speeds, feedforwards) -> setControl(
-            //         m_pathApplyRobotSpeeds.withSpeeds(speeds)
-            //             .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
-            //             .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
-            //     ),
-            //     new PPHolonomicDriveController(
-            //         // PID constants for translation
-            //         new PIDConstants(10, 0, 0),
-            //         // PID constants for rotation
-            //         new PIDConstants(7, 0, 0)
-            //     ),
-            //     config,
-            //     // Assume the path needs to be flipped for Red vs Blue, this is normally the case
-            //     () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
-            //     this // Subsystem for requirements
-            // );
             AutoBuilder.configure(
-                () -> RobotContainer.drivebase.getPose(),   // Supplier of current robot pose
+                () -> getState().Pose,   // Supplier of current robot pose
                 this::resetPose,         // Consumer for seeding pose against auto
                 () -> getState().Speeds, // Supplier of current robot speeds
                 // Consumer of ChassisSpeeds and feedforwards to drive the robot
@@ -243,6 +222,27 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
                 this // Subsystem for requirements
             );
+            // AutoBuilder.configure(
+            //     () -> RobotContainer.drivebase.getPose(),   // Supplier of current robot pose
+            //     this::resetPose,         // Consumer for seeding pose against auto
+            //     () -> getState().Speeds, // Supplier of current robot speeds
+            //     // Consumer of ChassisSpeeds and feedforwards to drive the robot
+            //     (speeds, feedforwards) -> setControl(
+            //         m_pathApplyRobotSpeeds.withSpeeds(speeds)
+            //             .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+            //             .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
+            //     ),
+            //     new PPHolonomicDriveController(
+            //         // PID constants for translation
+            //         new PIDConstants(10, 0, 0),
+            //         // PID constants for rotation
+            //         new PIDConstants(7, 0, 0)
+            //     ),
+            //     config,
+            //     // Assume the path needs to be flipped for Red vs Blue, this is normally the case
+            //     () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
+            //     this // Subsystem for requirements
+            // );
         } catch (Exception ex) {
             DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
         }
